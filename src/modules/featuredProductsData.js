0c8413f7a8cbf5e7d0cc33c011d23095d2a8fd4b
Artur Celer <acceler945@gmail.com>
2025-05-27 09:57:09 +0200

    Add featured products images and data

---
+++ src/modules/featuredProductsData.js
@@ -0,0 +1,241 @@
+// Normally I would fetch those products but the api format does not correspond to the required data format so featured products are hardcoded
+export const featuredProductsData = [
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+  {
+    name: 'Orange helmet for alpine TOUNDRA',
+    price: 300.00,
+    isLimited: true,
+    isBestseller: false,
+    photoUrl: '/images/products/helmet'
+  },
+  {
+    name: 'Green alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: false,
+    photoUrl: '/images/products/green-jacket'
+  },
+  {
+    name: 'Dark blue alpine climbing jacket',
+    price: 300.00,
+    isLimited: false,
+    isBestseller: true,
+    photoUrl: '/images/products/grey-jacket'
+  },
+]
