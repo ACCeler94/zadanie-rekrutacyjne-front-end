165a5c3a8185a1e325cf56cab491ac67366ca5d0
Artur Celer <acceler945@gmail.com>
2025-05-28 18:14:58 +0200

    Add initial fetch and styling

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 import { featuredProductsData } from './modules/featuredProductsData';
 import { initFeaturedProductsSwiper } from './modules/featuredProductsSwiper';
+import fetchProducts from './modules/fetchProducts';
+import { initCustomDropdown } from './modules/pageSizeMenu';
+
+
+let pageToFetch = 1;
+let pageSize = 14;
+let isFetching = false;
+let isError = false
+let allPages
+
+const productList = document.getElementById('product__grid');
+
 
 // Initialize swiper
 document.addEventListener('DOMContentLoaded', () => initFeaturedProductsSwiper(featuredProductsData))
+
+// Initialize page size menu
+const dropdownEl = document.getElementById("pageSizeDropdown");
+
+initCustomDropdown(dropdownEl, (value) => {
+  if (value === pageSize) return
+  // Reset page counter and refetch products when page size changes
+  pageSize = value
+  pageToFetch = 1
+  productList.innerHTML = ''
+  fetchProducts({ pageSize, pageToFetch, isFetching, isError, allPages })
+});
+
+// Initial fetch
+await fetchProducts({ pageSize, pageToFetch, isFetching, isError, allPages })
