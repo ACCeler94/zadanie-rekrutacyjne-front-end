9709f884d78f50c6c349bb021641cb1108cb3fde
Artur Celer <acceler945@gmail.com>
2025-05-28 17:37:36 +0200

    Add pageSizeMenu to select desired page size

---
+++ src/modules/pageSizeMenu.js
@@ -0,0 +1,34 @@
+export function initCustomDropdown(dropdownElement, onChange) {
+  const selected = dropdownElement.querySelector(".pageSizeMenu__dropdown--selected");
+  const valueSpan = dropdownElement.querySelector(".pageSizeMenu__dropdown--value");
+  const options = dropdownElement.querySelectorAll(".pageSizeMenu__dropdown--options li");
+
+  function updateHiddenOption(currentValue) {
+    options.forEach((opt) => {
+      opt.style.display = opt.dataset.value === currentValue ? "none" : "block";
+    });
+  }
+
+  selected.addEventListener("click", () => {
+    dropdownElement.classList.toggle("open");
+    updateHiddenOption(valueSpan.textContent);
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      valueSpan.textContent = option.dataset.value;
+      dropdownElement.classList.remove("open");
+      updateHiddenOption(option.dataset.value);
+      if (onChange) onChange(option.dataset.value);
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!dropdownElement.contains(e.target)) {
+      dropdownElement.classList.remove("open");
+    }
+  });
+
+  // Initial setup to hide selected option
+  updateHiddenOption(valueSpan.textContent);
+}
